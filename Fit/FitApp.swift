f52ae2103ab40aae5afece578ec184d8bf99237d
aelita27 <mukanbaevaaelita@gmail.com>
2024-12-20 18:30:29 +0600

    Initial Commit

---
+++ Fit/FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitApp.swift
+//  Fit
+//
+//  Created by aelita mukanbaeva on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
